065f8c973913f44ddfd02ff195fe8660831ea8a2
dcl21n15 <dcl21n15@iMac-de-iMac21.local>
2019-03-14 10:23:21 -0600

    problema 00

--- Medias.cs
+++ Medias.cs
@@ -11,21 +11,44 @@ namespace AutomatizarPruebasUnitarias {
         /**
          * Calcula y regresa la media artimética
          */
-        public static double mediaAritmetica(params int[] vals) { }
+        public static double mediaAritmetica(params int[] vals) {
+            double suma=0;
+            double media = 0; 
+
+            for (int i=0;i<vals.Lenght();i++){
+                suma=suma+vals[i];
+            }
+            media=suma/vals.Lenght();
+            return media;
+        }
 
         /**
          * Calcula y regresa la raíz enésima = x^(1/n)
          */
-        private static double raizEnesima(double x, int n) { }
+        private static double raizEnesima(double x, int n) { 
+            double raizEnesima = Math.Pow(x,n);
+            
+            return raizEnesima;
+        }
 
         /**
          *  Usa raizEnesima para calcular y regresar la media geométrica
          */
-        public double mediaGeometrica(params int[] vals) { }
+        public double mediaGeometrica(params int[] vals) {
+            double mediaGeometrica = 0;
+            double producto =0;
+            for (int i=0;i<vals.Lenght();i++){
+                producto=producto*vals[i];
+            }
+            mediaGeometrica= Math.Pow(producto,vals.Lenght());
+            return mediaGeometrica;
+         }
 
         /**
          * Este método no está implementado
          */
-        public static double mediaArmonica(params int[] vals) { }
+        public static double mediaArmonica(params int[] vals) { 
+
+        }
     }
 }
