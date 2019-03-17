18c6b8b4ce4863ed49b87b188f80d3a636ff7b20
AureAle <aperez33@ucol.mx>
2019-03-16 19:40:06 -0600

    problema 00

--- Medias.cs
+++ Medias.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +8,20 @@ namespace AutomatizarPruebasUnitarias {
     
     class Medias {
 
+        static void Main(){
+
+            int[] vals = new int[] { 2, 4, 8};
+            int[] geo = new int[] { 1, 2, 4, 8, 16, 32};
+            
+            Medias md = new Medias();
+            Console.WriteLine( "Media: " + mediaAritmetica(vals));
+            Console.WriteLine( "Raíz: " + raizEnesima(12,3));    
+            Console.WriteLine( "MediaGeométrica: " + md.mediaGeometrica(geo));
+
+
+
+        }
+
         /**
          * Calcula y regresa la media artimética
          */
@@ -15,18 +29,19 @@ namespace AutomatizarPruebasUnitarias {
             double suma=0;
             double media = 0; 
 
-            for (int i=0;i<vals.Lenght();i++){
+            for (int i=0;i<vals.Length;i++){
                 suma=suma+vals[i];
             }
-            media=suma/vals.Lenght();
+            media=suma/vals.Length;
             return media;
         }
 
         /**
          * Calcula y regresa la raíz enésima = x^(1/n)
          */
-        private static double raizEnesima(double x, int n) { 
-            double raizEnesima = Math.Pow(x,n);
+        private static double raizEnesima(double x, double n) { 
+            
+            double raizEnesima = Math.Pow(x,1/n);
             
             return raizEnesima;
         }
@@ -34,13 +49,16 @@ namespace AutomatizarPruebasUnitarias {
         /**
          *  Usa raizEnesima para calcular y regresar la media geométrica
          */
-        public double mediaGeometrica(params int[] vals) {
-            double mediaGeometrica = 0;
-            double producto =0;
-            for (int i=0;i<vals.Lenght();i++){
-                producto=producto*vals[i];
+        public  double mediaGeometrica(params int[] vals) {
+           
+            double producto =1;
+            double size = vals.Length;
+
+            for (int i=0;i<vals.Length;i++){
+               producto = producto * vals[i];
             }
-            mediaGeometrica= Math.Pow(producto,vals.Lenght());
+            
+            double mediaGeometrica= Math.Pow(producto,1/size);
             return mediaGeometrica;
          }
 
@@ -48,7 +66,8 @@ namespace AutomatizarPruebasUnitarias {
          * Este método no está implementado
          */
         public static double mediaArmonica(params int[] vals) { 
-
+        return 0;
         }
     }
+    
 }
